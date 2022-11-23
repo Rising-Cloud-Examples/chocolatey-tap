3f0f68a08606abee3af4702b8f00eef8e5cfff71
Eduardo Yanez <eduardo@risingcloud.com>
2022-11-22 22:05:08 -0600

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\risingcloud_x32.zip"
+  file64        = "$toolsDir\risingcloud_x64.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file,$packageArgs.file64
